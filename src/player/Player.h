ef379cc233f9ccd157c1d9769925b049afa90f52
Leland Sion <lelandsion@gmail.com>
2025-01-11 20:57:13 -0800

    feat(player): added features for collision handling, firing weapons, handling death and applying damage
    - Implemented firing of weapons logic
    - Implemented application of damage and handling of death
    - Adjusted HitBox positioning ot align visually with player

--- src/player/Player.h
+++ src/player/Player.h
@@ -17,36 +17,29 @@
 //   such as special abilities or character classes.
 // - Ensure that player controls and interactions are responsive and intuitive
 //   for an enhanced gameplay experience.
-
+// TODO: Add ability to change the keybindings
 #ifndef VIETNAM_MARINE_2_PLAYER_H
 #define VIETNAM_MARINE_2_PLAYER_H
 #pragma once
 
 // Includes to be added:
-/*
+
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <memory>
-#include "Weapon.h"
-#include "PowerUp.h"
-*/
+#include "PlayerEnums.h"
+#include "../weapon/Weapon.h"
+#include "../powerup/PowerUpTypeEnums.h"
+#include "../status_effect/StatusEffect.h"
+#include "../../include/Environment.h"
 
 class Player {
 public:
 
-    // Health states
-    enum class HealthState {
-        Happy,
-        RoughedUp,
-        Wounded,
-        Dying
-    };
-
-    // Add states for shooting, being hit, healing etc.
-
     // Constructors
     Player();
-    Player(const std::string &texturePath, sf::Vector2f startPosition, float initialSpeed, int initialHealth);
+
+    Player(sf::Vector2f startPosition, float initialSpeed, int initialHealth);
     Player(const Player &other);
     Player(Player &&other) noexcept;
 
@@ -54,29 +47,60 @@ public:
     ~Player();
 
     // Rendering
+    void initializeSprite(sf::Sprite &sprite, const sf::Texture &texture, const sf::Vector2f &offset, bool isAnimated = false);
     void render(sf::RenderWindow &window);
 
     // Input handling
-    void handleInput();
+    void handleInput(float deltaTime, const sf::RenderWindow &window);
 
-    // Update player state
-    void update(float deltaTime);
+    void updatePhysics(float deltaTime);
+    void updateEnvironmentInteraction(float deltaTime, const Environment& environment);
+    void updateAnimation(float deltaTime);
+    void updateMouseAim(const sf::Vector2i &mousePosition, const sf::RenderWindow &window);
+    void updateWeapons(float deltaTime);
 
     // Hitbox
     sf::FloatRect getHitbox() const;
+    sf::FloatRect getHeadHitBox() const;
+    sf::FloatRect getTorsoHitBox() const;
+    sf::FloatRect getLegsHitbox() const;
     sf::FloatRect getFeetHitbox() const;
 
-    // Health and health state
+    // Health
     int getHealth() const;
     void setHealth(int newHealth);
-    HealthState getHealthState() const;
-    void setHealthState(HealthState newState);jo
-    bool isAlive() const;
+    bool checkAlive() const;
 
-    // Inventory actions
+    // Location, physics
+    sf::Vector2f getPosition() const;
+    sf::Vector2f getVelocity() const;
+
+    // Player states
+    PlayerHealthState getHealthState() const;
+    void setHealthState(PlayerHealthState newState);
+    PlayerMovementDirection getMovementDirection() const;
+    void setMovementDirection(PlayerMovementDirection newState);
+    PlayerAction getActionState() const;
+    void setActionState(PlayerAction newState);
+
+    // Status effects
+    // void applyStatusEffect(StatusEffect effect, float duration, bool stackable = true);
+    void updateStatusEffects(float deltaTime);
+    // bool hasStatusEffect(StatusEffect effect) const;
+    // void removeStatusEffect(StatusEffect effect);
+
+    // Integrate status effects into health, and actions
+    void applyDamage(float amount);
+    void handleDeath();
+
+    // Weapons and inventory
     void addWeapon(std::shared_ptr<Weapon> weapon);
-    void switchWeapon(int index);
-    void fireCurrentWeapon();
+    void equipWeapon(int index);
+    void fireCurrentWeapon(const sf::Vector2<float> &direction, const sf::Vector2<float> &swivelPoint);
+    void setWeaponAmmo(int weaponIndex, int newAmmo);
+    int getCurrentWeaponIndex();
+    std::vector<std::shared_ptr<Weapon>> getWeapons();
+    std::vector<std::shared_ptr<Projectile>> getAllProjectiles() const;
 
     // Score
     int getLevelScore() const;
@@ -87,9 +111,49 @@ public:
 
 private:
     // Rendering
-    sf::Sprite playerSprite;        // Player sprite for rendering
-    sf::Texture playerTexture;      // Texture for the player sprite
-    sf::IntRect animationFrame;     // Rectangle defining current animation frame
+    sf::Sprite playerSprite;        // Player sprite for rendering ** to be deleted **
+    sf::Texture playerTexture;      // Texture for the player sprite this will be a sheet
+    sf::Sprite deathSprite;        // Player sprite for rendering ** to be deleted **
+    sf::Texture deathTexture;      // Texture for the player sprite this will be a sheet
+
+    sf::Sprite torsoSprite;
+    sf::Sprite headSprite;
+    sf::Sprite armsSprite;
+    sf::Sprite leftArmsSprite;
+    sf::Sprite legsSprite;
+
+    sf::Texture torsoTexture;
+    sf::Texture headTexture;
+    sf::Texture armsTexture;
+    sf::Texture leftArmsTexture;
+    sf::Texture legsTexture;
+
+    sf::Vector2f position;       // Base position
+    sf::Vector2f torsoOffset;    // Offset for the torso
+    sf::Vector2f headOffset;     // Offset for the head
+    sf::Vector2f armsOffset;     // Offset for the arms
+    sf::Vector2f leftArmsOffset;     // Offset for the arms
+    sf::Vector2f legsOffset;     // Offset for the legs
+
+    // stateDelay
+    sf::Clock animationClock;        // Clock to track elapsed time
+    float idleDelayTimer;
+    float stateDelay;
+    float fireDelayTimer;
+    float fireDelay;
+
+    // Animations
+    sf::Vector2i currentFrame;       // Current frame of the animation (x, y index in the sprite sheet)
+    sf::Vector2i frameSize;          // Size of each frame in the sprite sheet
+    sf::Vector2i deathFrameSize;          // Size of each frame in the sprite sheet
+
+    float frameTime;                 // Time per frame (seconds)
+    float elapsedTime;               // Accumulator for frame switching
+    sf::IntRect frameRect;           // rectangle for the current frame and displaying the sprite
+    sf::IntRect deathFrameRect;           // rectangle for the current frame and displaying the sprite
+    sf::IntRect legsFrameRect;       // rectangle for the current frame for the legs and displaying for the legs
+    bool jumpAnimationStarted;           // Flag for when the jumping animation has started or not
+    bool isFlipped;
 
     // Movement
     void moveLeft(float deltaTime);
@@ -97,28 +161,47 @@ private:
     void jump();
     void duck();
 
-    // Position and attributes
-    sf::Vector2f position;          // Player position
+    // Physics and attributes
     sf::Vector2f velocity;          // Player movement velocity
     float speed;                    // Movement speed
     float jumpHeight;               // Height for jumping
 
-    // Player state
+    bool isJumping;                 // Whether the player is currently jumping
+    bool isFalling;                 // Whether the player is falling check this
+    bool isDucking;                 // Whether the player is ducking
+    bool isOnGround;                // Whether the player is onGround
+    bool mousePressed;              // Whether the mouse is being held
+
+    // Health
     int health;                     // Current health of the player
     int maxHealth;                  // Maximum health of the player
     bool isAlive;                   // Whether the player is alive
-    bool isJumping;                 // Whether the player is currently jumping
-    bool isFalling;                 // Whether the player is falling
-    bool isDucking;                 // Whether the player is ducking
+
+    // States
+    PlayerState playerState;
+    PlayerHealthState playerHealthState;
+    PlayerMovementDirection playerMovementDirection;
+    PlayerAction playerAction;
+    PlayerWeaponState playerWeaponState;
+    PlayerArmorType playerArmorType;
+    PlayerInventoryType playerInventoryType;
+    PlayerClass playerClass;
+
+    // Status effects
+    StatusEffectManager statusManager;
 
     // Hitboxes
     void updateHitboxes();
     sf::FloatRect hitbox;           // Main hitbox for the player
+    sf::FloatRect headHitbox;           // Main hitbox for the player
+    sf::FloatRect torsoHitbox;           // Main hitbox for the player
+    sf::FloatRect legsHitbox;           // Main hitbox for the player
     sf::FloatRect feetHitbox;       // Smaller hitbox for ground detection
 
     // Inventory and weapons
     std::vector<std::shared_ptr<Weapon>> weapons; // List of player's weapons
-    std::vector<PowerUp> powerUps;                // List of power-ups
+    std::shared_ptr<Weapon> currentWeapon = nullptr;
+    // std::vector<PowerUp> powerUps;                // List of power-ups
     int currentWeaponIndex;                       // Index of currently selected weapon
 
     // Player stats
